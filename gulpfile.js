80937143705dd51e86cad2c431136c20504fd6c4
Christian Freear <christianfreear@gmail.com>
2014-09-16 19:00:33 +0100

    development gulpfile, jshint and build tasks. watch to come.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+// include gulp
+var gulp = require('gulp');
+
+// include plug-ins
+var jshint = require('gulp-jshint');
+
+var stripDebug = require('gulp-strip-debug');
+var uglifyjs = require('gulp-uglifyjs');
+
+// JS hint task
+gulp.task('jshint', function() {
+    gulp.src('./src/jquery.textlimit.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Build task
+gulp.task('build', function() {
+    gulp.src('./src/jquery.textlimit.js')
+        .pipe(stripDebug())
+        .pipe(uglifyjs('jquery.textlimit.min.js', {
+            'outSourceMap': true
+        }))
+        .pipe(gulp.dest('./build/'))
+});
