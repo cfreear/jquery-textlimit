2bf9857253284e94bb2f3b9f92218bd0742b9047
Christian Freear <christianfreear@gmail.com>
2014-09-18 21:48:54 +0100

    Travis CI integration

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,28 @@ var jshint = require('gulp-jshint');
 var stripDebug = require('gulp-strip-debug');
 var uglifyjs = require('gulp-uglifyjs');
 
+var qunit = require('gulp-qunit');
+
 // JS hint task
 gulp.task('jshint', function() {
-    gulp.src('./src/jquery.textlimit.js')
+    return gulp.src('./src/jquery.textlimit.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Build task
 gulp.task('build', function() {
-    gulp.src('./src/jquery.textlimit.js')
+    return gulp.src('./src/jquery.textlimit.js')
         .pipe(stripDebug())
         .pipe(uglifyjs('jquery.textlimit.min.js', {
             'outSourceMap': true
         }))
-        .pipe(gulp.dest('./build/'))
+        .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('travis' function() {
+    return gulp.src('./tests/index.html')
+        .pipe(qunit());
 });
 
 //default task, run all and then watch
